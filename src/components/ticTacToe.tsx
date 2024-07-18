8b191e24404757d6fd6d8a49dead5a6e792e46cb
Nicolau <nicodinizmoraes@gmail.com>
2024-07-18 15:59:19 -0300

    feat: basic layout

--- src/components/ticTacToe.tsx
+++ src/components/ticTacToe.tsx
@@ -2,5 +2,23 @@ import "./ticTacToe.css";
 
 
 export function TicTacToe() {
-    return <div class="container">TicTacToe</div>;
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+            </div>
+            <div class="row">
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+            </div>
+            <div class="row">
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+            </div>
+        </div>
+    );
 }
